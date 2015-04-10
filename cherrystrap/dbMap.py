316d731e62c78a3bea41098150db979d1cd2b92e
Justin Evans <justin.evans@gmail.com>
2015-04-10 09:00:57 -0700

    Added database versions for Isengard

--- cherrystrap/dbMap.py
+++ cherrystrap/dbMap.py
@@ -9,6 +9,12 @@ def table_map(version):
 		table_list['artistinfo'] = music_database+'.artistinfo'
 		table_list['albuminfo'] = music_database+'.albuminfo'
 
+		table_list['movieview'] = video_database+'.movieview'
+		table_list['tvshowview'] = video_database+'.tvshowview'
+		table_list['seasonview'] = video_database+'.seasonview'
+		table_list['episodeview'] = video_database+'.episodeview'
+		table_list['musicvideoview'] = video_database+'.musicvideoview'
+
 	elif version == "Gotham":
 		music_database = 'xbmc_music46'
 		video_database = 'xbmc_video78'
@@ -16,12 +22,37 @@ def table_map(version):
 		table_list['artistinfo'] = music_database+'.artist'
 		table_list['albuminfo'] = music_database+'.album'
 
+		table_list['movieview'] = video_database+'.movieview'
+		table_list['tvshowview'] = video_database+'.tvshowview'
+		table_list['seasonview'] = video_database+'.seasonview'
+		table_list['episodeview'] = video_database+'.episodeview'
+		table_list['musicvideoview'] = video_database+'.musicvideoview'
+
 	elif version == "Helix":
 		music_database = 'xbmc_music48'
 		video_database = 'xbmc_video90'
 
 		table_list['artistinfo'] = music_database+'.artist'
 		table_list['albuminfo'] = music_database+'.album'
+
+		table_list['movieview'] = video_database+'.movieview'
+		table_list['tvshowview'] = video_database+'.tvshowview'
+		table_list['seasonview'] = video_database+'.seasonview'
+		table_list['episodeview'] = video_database+'.episodeview'
+		table_list['musicvideoview'] = video_database+'.musicvideoview'
+
+	elif version == "Isengard":
+		music_database = 'xbmc_music49'
+		video_database = 'xbmc_video91'
+
+		table_list['artistinfo'] = music_database+'.artist'
+		table_list['albuminfo'] = music_database+'.album'
+
+		table_list['movieview'] = video_database+'.movie_view'
+		table_list['tvshowview'] = video_database+'.tvshow_view'
+		table_list['seasonview'] = video_database+'.season_view'
+		table_list['episodeview'] = video_database+'.episode_view'
+		table_list['musicvideoview'] = video_database+'.musicvideo_view'
 
 	#Common
 	table_list['music_art'] = music_database+'.art'
@@ -35,14 +66,9 @@ def table_map(version):
 
 	table_list['video_art'] = video_database+'.art'
 	table_list['movie'] = video_database+'.movie'
-	table_list['movieview'] = video_database+'.movieview'
 	table_list['tvshow'] = video_database+'.tvshow'
-	table_list['tvshowview'] = video_database+'.tvshowview'
 	table_list['seasons'] = video_database+'.season'
-	table_list['seasonview'] = video_database+'.seasonview'
 	table_list['episode'] = video_database+'.episode'
-	table_list['episodeview'] = video_database+'.episodeview'
 	table_list['musicvideo'] = video_database+'.musicvideo'
-	table_list['musicvideoview'] = video_database+'.musicvideoview'
 
 	return table_list
